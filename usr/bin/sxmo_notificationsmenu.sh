c24b0071d1972281e77baed428c8edee6998182a
Christian Immanuel Fischer <mail@christianimmanuel.de>
2021-06-09 10:14:30 +0200

    wofi dmenu as main Menu 	modified:   home/config/waybar/config_0 	new file:   usr/bin/sxmo_notificationsmenu.sh 	new file:   usr/local/bin/swayphone_appmenu

---
+++ usr/bin/sxmo_notificationsmenu.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env sh
+
+# include common definitions
+# shellcheck source=scripts/core/sxmo_common.sh
+. "$(dirname "$0")/sxmo_common.sh"
+
+notificationmenu() {
+	CHOICES="$icon_cls Close Menu\n$icon_del Clear Notifications"
+	# shellcheck disable=SC2045
+	for NOTIFFILE in $(ls -tr "$NOTIFDIR"); do
+		NOTIFMSG="$(tail -n+3 "$NOTIFDIR/$NOTIFFILE" | tr "\n^" " ")"
+		NOTIFHRANDMIN="$(stat --printf %y "$NOTIFDIR/$NOTIFFILE" | grep -oE '[0-9]{2}:[0-9]{2}')"
+		CHOICES="
+			$CHOICES
+			$NOTIFHRANDMIN - $NOTIFMSG ^ $NOTIFDIR/$NOTIFFILE
+		"
+	done
+
+	PICKEDCONTENT="$(
+		printf "%b" "$CHOICES" |
+		xargs -0 echo |
+		sed '/^[[:space:]]*$/d' |
+		awk '{$1=$1};1' |
+		cut -d^ -f1 |
+		wofi --width="90%" --height="54%" -S dmenu -c -i -p "Notifs" -l 20
+	)"
+
+	echo "$PICKEDCONTENT" | grep -q "Close Menu" && exit 1
+	echo "$PICKEDCONTENT" | grep -q "Clear Notifications" && rm "$NOTIFDIR"/* && exit 1
+
+	PICKEDNOTIFFILE="$(echo "$CHOICES" | tr -s ' ' | grep -F "$PICKEDCONTENT" | cut -d^ -f2 | tr -d ' ')"
+	NOTIFACTION="$(head -n1 "$PICKEDNOTIFFILE")"
+	setsid -f sh -c "$NOTIFACTION" &
+	rm "$PICKEDNOTIFFILE"
+}
+
+notificationmenu
